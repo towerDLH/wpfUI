cfc380065605be767ada72e76edfb099c0adcc9d
1341105324 <1341105324@qq.com>
2020-06-24 14:47:08 +0800

    流程图

--- WpfApp3/WPfTestContrl/FlowWindow.xaml.cs
+++ WpfApp3/WPfTestContrl/FlowWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DiagramDesigner.Controls;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,11 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace WpfApp3.WPfTestContrl
 {
@@ -23,5 +27,83 @@ namespace WpfApp3.WPfTestContrl
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// 保存子节点
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSaveChild_Click(object sender, RoutedEventArgs e)
+        {
+             
+            IEnumerable<DesignerItem> designerItems = MyDesigner.Children.OfType<DesignerItem>();
+            IEnumerable<Connection> connections = MyDesigner.Children.OfType<Connection>();
+
+            XElement designerItemsXML = SerializeDesignerItems(designerItems);
+            XElement connectionsXML = SerializeConnections(connections);
+
+            XElement root = new XElement("Root");
+            root.Add(designerItemsXML);
+            root.Add(connectionsXML);
+
+            SaveFile(root);
+        }
+
+        private XElement SerializeConnections(IEnumerable<Connection> connections)
+        {
+            var serializedConnections = new XElement("Connections",
+                           from connection in connections
+                           select new XElement("Connection",
+                                      new XElement("SourceID", connection.Source.ParentDesignerItem.ID),
+                                      new XElement("SinkID", connection.Sink.ParentDesignerItem.ID),
+                                      new XElement("SourceConnectorName", connection.Source.Name),
+                                      new XElement("SinkConnectorName", connection.Sink.Name),
+                                      new XElement("SourceArrowSymbol", connection.SourceArrowSymbol),
+                                      new XElement("SinkArrowSymbol", connection.SinkArrowSymbol),
+                                      new XElement("zIndex", Canvas.GetZIndex(connection))
+                                     )
+                                  );
+
+            return serializedConnections;
+        }
+
+        private XElement SerializeDesignerItems(IEnumerable<DesignerItem> designerItems)
+        {
+            XElement serializedItems = new XElement("DesignerItems",
+                                       from item in designerItems
+                                       let contentXaml = XamlWriter.Save(((DesignerItem)item).Content)
+                                       select new XElement("DesignerItem",
+                                                  new XElement("Left", Canvas.GetLeft(item)),
+                                                  new XElement("Top", Canvas.GetTop(item)),
+                                                  new XElement("Width", item.Width),
+                                                  new XElement("Height", item.Height),
+                                                  new XElement("ID", item.ID),
+                                                  new XElement("zIndex", Canvas.GetZIndex(item)),
+                                                  new XElement("IsGroup", item.IsGroup),
+                                                  new XElement("ParentID", item.ParentID),
+                                                  new XElement("Content", contentXaml)
+                                              )
+                                   );
+
+            return serializedItems;
+        }
+
+        void SaveFile(XElement xElement)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Files (*.xml)|*.xml|All Files (*.*)|*.*";
+            if (saveFile.ShowDialog() == true)
+            {
+                try
+                {
+                    xElement.Save(saveFile.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.StackTrace, ex.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        
     }
 }
